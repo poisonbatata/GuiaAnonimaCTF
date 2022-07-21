f9bcb8d8476a93e3cce7d041ce5c178e4675992c
PoisON <86176940+poisonbatata@users.noreply.github.com>
2022-07-21 11:50:11 -0300

    Update c!newctf
    Agora os parâmetros não possuem uma ordem obrigatória predefinida. O usuário poderá utilizar o comando c!newctf com os parâmetros em qualquer ordem.

--- main.py
+++ main.py
@@ -21,32 +21,36 @@ async def start(ctx):
 
 @bot.command(name="newctf", help='Cria um embed sobre o novo ctf.\nUso: c!newctf --name Expertum --difficulty Easy --flags 3 --time "1 Semana (28 abr 2022 18:00 até 05 mai 2022 18:00)" --hints 3 --delay 2d', brief="Cria um embed sobre o novo ctf.")
 # c!newctf : Manda a mensagem embed sobre o novo ctf
-async def embedCtf(ctx, *args):
+async def embedCtf(ctx, *args): # Colocar opção para qnd n houver argumentos
     argsList = []
     for arg in args:
         argsList.append(arg)
-    if argsList[0] == "--name" or argsList[0] == "-n":
-        nome = argsList[1]
-    if argsList[2] == "--difficulty" or argsList[2] == "-d":
-        dificuldade = argsList[3]
-    if argsList[4] == "--flags" or argsList[4] == "-f":
-        flags = argsList[5]
-    if argsList[6] == "--time" or argsList[6] == "-t":
-        duracao = argsList[7]
-    if argsList[8] == "--hints" or argsList[8] == "-h":
-        if argsList[9] == '0':
-            hints="Sem hints."
-        elif argsList[9] == '1':
-            hints="Hint: {hint1}"
-        elif argsList[9] == '2':
-            hints="""1º Hint: {hint1}
-    2º Hint: {hint2}"""
-        elif argsList[9] == '3':
-            hints="""1º Hint: {hint1}
-    2º Hint: {hint2}
-    3º Hint: {hint3}"""
-        elif argsList[9] != 1 and 2 and 3:
-            hints='-'
+
+    for count, value in enumerate(argsList):
+        if value == "--name" or value == "-n":
+            nome = argsList[count+1]
+        if value == "--difficulty" or value == "-d":
+            dificuldade = argsList[count+1]
+        if value == "--flags" or value == "-f":
+            flags = argsList[count+1]
+        if value == "--time" or value == "-t":
+            duracao = argsList[count+1]
+        
+        if value == "--hints" or value == "-h":
+            flags = argsList[count+1]
+            if argsList[9] == '0':
+                hints="Sem hints."
+            elif argsList[9] == '1':
+                hints="Hint: {hint1}"
+            elif argsList[9] == '2':
+                hints="""1º Hint: {hint1}
+        2º Hint: {hint2}"""
+            elif argsList[9] == '3':
+                hints="""1º Hint: {hint1}
+        2º Hint: {hint2}
+        3º Hint: {hint3}"""
+            elif argsList[9] != 1 and 2 and 3:
+                hints='-'
 
     descricao=f'Neste desafio você tem que capturar {flags} FLAGS! Temos uma plataforma para ver os desafios, hints, pontuar as flags e muito mais! Basta você se registrar na plataforma para jogar e clicar na aba "Challenges" para ver os desafios.'
 
